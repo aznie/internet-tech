d95553268c9221a251208a34c14b3387c948246c
Alexandr Zlobin <aleksandr.zlobin@nixsolutions.com>
2020-11-23 20:45:09 +0200

    JS refactoring

--- itech_3/script.js
+++ itech_3/script.js
@@ -1,24 +1,24 @@
 const handleSumbit = () => {
-  const img = document.getElementById('img');
-  const width = document.getElementsByClassName("width-settings")[0].value + "px";
-  const height = document.getElementsByClassName("height-settings")[0].value + "px";
-  const altText = document.getElementsByClassName("alt-text")[0].value
-  const borderWidth = document.getElementsByClassName("border-width")[0].value + "px";
+  const img = document.querySelector('#img');
+  const width = document.querySelectorAll('.width-settings')[0].value + "px";
+  const height = document.querySelectorAll('.height-settings')[0].value + "px";
+  const altText = document.querySelectorAll('.alt-text')[0].value
+  const borderWidth = document.querySelectorAll('.border-width')[0].value + "px";
   const border = `solid ${borderWidth} rgb(8, 88, 88)`;
   img.style.width = width;
   img.style.height = height;
   img.style.border = border;
-  img.setAttribute("alt", altText.toString());
+  img.setAttribute('alt', altText.toString());
 }
 
 const handleSelect = () => {
-  const sel = document.getElementById('selector').selectedIndex;
-  const options = document.getElementById('selector').options;
-  const img = document.getElementById('img');
+  const sel = document.querySelector('#selector').selectedIndex;
+  const options = document.querySelector('#selector').options;
+  const img = document.querySelector('#img');
   const newImg = new Image();
   newImg.src = options[sel].value;
 
   newImg.onload = () => {
-    img.setAttribute("src", options[sel].value);
+    img.setAttribute('src', options[sel].value);
   }
 }
